3cba6e4145916dae1157b2e24434b4b2763f5a5b
Galaticos402 <64137012+Galaticos402@users.noreply.github.com>
2024-02-21 18:15:15 +0700

    Add Infrastructure project

---
+++ Core/Migrations/20240221110850_UpdateUserDiscriminator.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Core.Migrations
+{
+    public partial class UpdateUserDiscriminator : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Discriminator",
+                table: "Users",
+                newName: "Role");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Role",
+                table: "Users",
+                newName: "Discriminator");
+        }
+    }
+}
